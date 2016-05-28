72ec67c03195e68bcf3f14dc277a1ee744805856
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-05-28 08:07:33 +0430

    add image logic add image model extra methods add todo unittest on image logic

--- backend/app/Image.php
+++ backend/app/Image.php
@@ -32,4 +32,59 @@ class Image extends Model
 	{
 		return $this->belongsTo('App\InstagramProfile', 'profile_id', 'profile_id');
 	}
+
+    /**
+     * Scope a query to get next image id.
+     *
+     * @param  collection $query
+     * @param  datetime $createdTime
+     * @param  string  $state image state, show|hide|new, default show
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+	public function scopeNextId($query, $createdTime, $state)
+	{
+		return $query->whereRaw("created_time > STR_TO_DATE('" . $createdTime . "', '%Y-%m-%d %H:%i:%s')")
+					 ->WhereStateOrderByCreatedTime($state, 'asc')
+					 ->first(['id']);
+	}
+
+    /**
+     * Scope a query to get prev image id.
+     *
+     * @param  collection $query
+     * @param  datetime $createdTime
+     * @param  string  $state image state, show|hide|new, default show
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+	public function scopePrevId($query, $createdTime, $state)
+	{
+		return $query->whereRaw("created_time < STR_TO_DATE('" . $createdTime . "', '%Y-%m-%d %H:%i:%s')")
+					 ->WhereStateOrderByCreatedTime($state)
+					 ->first(['id']);
+	}
+
+    /**
+     * Scope a query to get by state and order.
+     * 
+     * @param  collection $query
+     * @param  string  $state image state, show|hide|new, default show
+     * @param  string $order
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+	public function scopeWhereStateOrderByCreatedTime($query, $state, $order = 'desc')
+	{
+		return $query->where('state', '=', $state)
+					 ->orderBy('created_time', $order)
+					 ->orderBy('created_at', $order);
+	}
+
+	public function getFullAttribute($value)
+	{
+		return asset('app/' . $value);
+	}
+
+	public function getThumbAttribute($value)
+	{
+		return asset('app/' . $value);
+	}
 }
---
+++ backend/tests/ImageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Helpers\Image;
+use App\InstagramProfile;
+use App\Image as ImageModel;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class InstagramTest extends TestCase
+{
+    public function testAll()
+    {
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    public function testUpdate()
+    {
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+}
