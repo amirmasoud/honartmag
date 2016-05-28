fb21b4aa5d09b82c30d300e73a1632230f26c0ac
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-05-28 08:11:22 +0430

    add images:update and images:store commands add daily schedule for running images:update on every instagram profile id

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        \App\Console\Commands\StoreImages::class,
+        \App\Console\Commands\UpdateImages::class,
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('images:update')
+                 ->daily();
     }
 }
