1ebe8129d7be18a61f9c62e635f914b69bcd6ef5
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-06-02 19:29:29 +0430

    add category model for image and seed/factory

---
+++ backend/database/migrations/2016_06_01_121739_add_category_id_to_images.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoryIdToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')
+                  ->references('id')->on('categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropForeign('images_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+}
